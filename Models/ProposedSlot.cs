8e83db49f232dbe676759d051557336b701ad479
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-01 21:09:31 +0900

    Create ProposedSlot.cs

---
+++ Models/ProposedSlot.cs
@@ -0,0 +1,42 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MeetingScheduler.Models;
+
+public class SlotResponse
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserId { get; set; } = string.Empty;
+    
+    public string Response { get; set; } = string.Empty;
+    public DateTime RespondedAt { get; set; } = DateTime.UtcNow;
+}
+
+public class ProposedSlot
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+    
+    [BsonElement("meeting_id")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string MeetingId { get; set; } = string.Empty;
+    
+    [BsonElement("start_time")]
+    public DateTime StartTime { get; set; }
+    
+    [BsonElement("end_time")]
+    public DateTime EndTime { get; set; }
+    
+    [BsonElement("ai_score")]
+    public double AiScore { get; set; }
+    
+    [BsonElement("is_finalized")]
+    public bool IsFinalized { get; set; } = false;
+    
+    [BsonElement("responses")]
+    public List<SlotResponse> Responses { get; set; } = new();
+    
+    [BsonElement("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
