8ffba01772c0f5e549948d874eda022a2b8abdd3
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-02 02:19:48 +0900

    Update LoginRequest.cs

--- DTOs/LoginRequest.cs
+++ DTOs/LoginRequest.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace MeetingScheduler.DTOs;
 
 public class LoginRequest
 {
+    [JsonPropertyName("email")]
     public string Email { get; set; } = string.Empty;
+    
+    [JsonPropertyName("password")]
     public string Password { get; set; } = string.Empty;
 }
