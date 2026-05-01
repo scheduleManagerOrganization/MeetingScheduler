55e20c332fb11fb67a918bfc678b5701785fd4c1
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-01 21:04:38 +0900

    Create RespondSlotRequest.cs

---
+++ DTOs/RespondSlotRequest.cs
@@ -0,0 +1,8 @@
+namespace MeetingScheduler.DTOs;
+
+public class RespondSlotRequest
+{
+    public string SlotId { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string Response { get; set; } = string.Empty;  // 'yes', 'no', 'maybe'
+}
