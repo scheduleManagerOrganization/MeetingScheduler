579351c4722a7f8f042676a7fd94a1141f8ac589
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-01 22:50:12 +0900

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@ namespace MeetingScheduler.Models;
 public class User
 {
     [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = string.Empty;
     
     [BsonElement("email")]
