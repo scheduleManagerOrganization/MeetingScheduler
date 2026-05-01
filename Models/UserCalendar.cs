a218ed388ac7d1dd878ee554eb44c0270fec6a9c
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-01 21:10:23 +0900

    Create UserCalendar.cs

---
+++ Models/UserCalendar.cs
@@ -0,0 +1,30 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MeetingScheduler.Models;
+
+public class TimeSlot
+{
+    public string Start { get; set; } = string.Empty;
+    public string End { get; set; } = string.Empty;
+}
+
+public class UserCalendar
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+    
+    [BsonElement("user_id")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserId { get; set; } = string.Empty;
+    
+    [BsonElement("date")]
+    public string Date { get; set; } = string.Empty;
+    
+    [BsonElement("slots")]
+    public List<TimeSlot> Slots { get; set; } = new();
+    
+    [BsonElement("updated_at")]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
