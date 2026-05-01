9c8097056f9eb82bd239655a8a0dc1f857fa7b2d
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-01 21:03:58 +0900

    Create JoinTeamRequest.cs

---
+++ DTOs/JoinTeamRequest.cs
@@ -0,0 +1,7 @@
+namespace MeetingScheduler.DTOs;
+
+public class JoinTeamRequest
+{
+    public string JoinCode { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
